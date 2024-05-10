abfdb7ff34feef261f7c48c8a5e5c9655110652a
Akshay <akshay@cloudrevelinnovation.com>
2024-05-10 18:39:26 +0530

    settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,19 @@
-<div class="min-h-screen bg-gray-100">
-    @include('layouts.navigation')
-
-    <!-- Page Heading -->
-    <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            {{ $header }}
-        </div>
-    </header>
-
+<div class="flex flex-col h-screen bg-gray-100">
     <!-- Page Content -->
-    <main>
-        {{ $slot }}
+    <main class="flex flex-1">
+        <!-- Sidebar -->
+        @include('layouts.admin.sidebar')
+
+        <!-- Page Content -->
+        <div class="flex-1 overflow-y-auto">
+            @include('layouts.navigation')
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+            <!-- Your main content here -->
+            {{ $slot }}
+        </div>
     </main>
 </div>
